3516db3cb64d4100e5803425457de114e323cbfd
Josep Valls <alfredvays@gmail.com>
2023-03-29 00:00:42 +0200

    Start app  Changes to be committed: 	new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app.ts 	renamed:    typescript1/tsconfig.json -> tsconfig.json

--- typescript1/dist/type_assertion.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Type assertion
-let channel = 'Josep';
-// let channelStr = <string>channel;
-let channelStr = channel;
-//const myCanvas = document.getElementById('main') as HTMLCanvasElement;
-const myCanvas = document.getElementById('main');
--- typescript1/src/type_assertion.ts
+++
@@ -1,9 +0,0 @@
-// Type assertion
-let channel: any = 'Josep';
-
-// let channelStr = <string>channel;
-
-let channelStr = channel as string;
-
-//const myCanvas = document.getElementById('main') as HTMLCanvasElement;
-const myCanvas = <HTMLCanvasElement>document.getElementById('main');
