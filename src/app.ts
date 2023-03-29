f8004083c87618afeaf3dce888267f2a6cf9d2a6
Josep Valls <alfredvays@gmail.com>
2023-03-29 17:41:55 +0200

    Routes and connection DB  Changes to be committed: 	modified:   src/app.ts 	new file:   src/config/mongo.ts 	new file:   src/routes/blogs.ts 	new file:   src/routes/index.ts 	new file:   src/routes/item.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { router } from "./routes";
+import db from "./config/mongo"
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
 
+app.use(router)
+db().then(() => console.log("Conexion to DB OK..."))
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
 
