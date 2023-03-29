2718d2b7062d13490bb975b8d678da6151dfc9f8
Josep Valls <alfredvays@gmail.com>
2023-03-29 17:55:03 +0200

    Controller items base

---
+++ src/controllers/item.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+
+export const getItem = (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({error})
+    }
+}
+
+export const getItems = (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const createItem = (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const updateItem = (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const deleteItem = (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
