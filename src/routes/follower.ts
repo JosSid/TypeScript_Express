770db3e216bea006d2d7f1c16c5070656caba55d
Josep Valls <alfredvays@gmail.com>
2023-03-30 12:12:34 +0200

    logMiddleware  Changes to be committed: 	new file:   src/middleware/log.ts 	modified:   src/routes/follower.ts

--- src/routes/follower.ts
+++ src/routes/follower.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
 import { createItem, deleteItem, getItem, getItems, updateItem } from "../controllers/followers";
+import { logMiddleware } from "../middleware/log";
 
 const router = Router();
 
-router.get("/", getItems);
+router.get("/", logMiddleware, getItems);
 router.get("/:id", getItem);
 
 router.post("/", createItem);
