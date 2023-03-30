2d3ca4e00c7a57a7ae96366337faacfe13d41a21
Josep Valls <alfredvays@gmail.com>
2023-03-30 18:02:44 +0200

    password encripted in signup user  Changes to be committed: 	modified:   src/controllers/auth.ts 	modified:   src/services/authService.ts 	new file:   src/utils/bcrypt_handle.ts

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,19 @@
-import { Request, Response } from "express";
-import { signupUser, loginUser } from "../services/authService";
-const signupController = async ({body}: Request, res: Response) => {
-    const responseUser = await signupUser(body)
+import { Request, Response } from 'express';
+import { signupUser, loginUser } from '../services/authService';
+import { handleHttp } from '../utils/error_handle';
+const signupController = async ({ body }: Request, res: Response) => {
+  try {
+    const responseUser = await signupUser(body);
+    if(responseUser === "ALREADY_USER"){
+        res.status(409).json({error: responseUser});
+        return;
+    }
+    res.status(201).json({ data: responseUser });
+  } catch (error) {
+    handleHttp(res, 'ERROR_CREATE_ITEM', error);
+  }
 };
 
-const loginController = async (req: Request, res: Response) => {
+const loginController = async (req: Request, res: Response) => {};
 
-};
-
-export { signupController, loginController};
+export { signupController, loginController };
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,13 @@
 import { User } from "../interfaces/user.interface";
 import UserModel from "../models/user";
+import { encrypt } from "../utils/bcrypt_handle";
 
-const signupUser = async ({ mail, password, name }: User) => {
+const signupUser = async ({ mail, password, name, comments }: User) => {
     const checkIs = await UserModel.findOne({ mail });
     if(checkIs)  return "ALREADY_USER";
 
-    const registerNewUser = await UserModel.create({ mail, password, name })
+    const passHash = await encrypt(password)
+    const registerNewUser = await UserModel.create({ mail, password: passHash , name, comments });
 
     return registerNewUser;
 };
